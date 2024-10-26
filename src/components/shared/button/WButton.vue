4754e281dd78dde8e364966d0384df28419369f3
ArviinM <rhenmedina20@gmail.com>
2024-10-26 11:40:06 +0800

    chore: added shared components for button, input, and label

---
+++ src/components/shared/button/WButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { buttonVariants } from '@/components/shared/button/index' // Import cva
+
+const props = defineProps<{
+  type?: 'button' | 'submit' | 'reset'
+  variant?: 'default' | 'outline' | 'ghost'
+  size?: 'default' | 'sm' | 'lg'
+  disabled?: boolean
+  class?: string
+}>()
+
+const buttonClasses = computed(() =>
+  buttonVariants({
+    variant: props.variant,
+    size: props.size,
+    class: props.class,
+  }),
+)
+</script>
+
+<template>
+  <button :type="props.type" :class="buttonClasses" :disabled="props.disabled">
+    <slot />
+  </button>
+</template>
---
+++ src/components/shared/label/WLabel.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  usage: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <label
+    class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+    :for="usage"
+  >
+    <slot />
+  </label>
+</template>
