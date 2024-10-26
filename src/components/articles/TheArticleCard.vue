7baa0fdc619b4b4cbecd3256688e9f81465515a8
ArviinM <rhenmedina20@gmail.com>
2024-10-26 14:10:09 +0800

    chore: added new article card

---
+++ src/components/articles/TheArticleCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  imageUrl: string
+  date: string
+  link: string
+  writerId?: number
+  editorId?: number
+  writerName?: string
+  editorName?: string
+}>()
+</script>
+
+<template>
+  <div class="border rounded-lg p-4 shadow-md">
+    <img
+      :src="imageUrl"
+      alt="Article Image"
+      class="mb-2 rounded-md h-56 w-full object-cover"
+    />
+    <h3 class="text-lg font-medium text-gray-900">{{ title }}</h3>
+    <h4 class="text-md font-medium text-gray-900">{{ link }}</h4>
+    <p class="text-gray-500">
+      {{ date }} - <span v-if="writerId">Writer: {{ writerName }}</span>
+      <span v-if="editorId"> Editor: {{ editorName }}</span>
+    </p>
+  </div>
+</template>
